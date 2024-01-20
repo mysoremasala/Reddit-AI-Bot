d3b671f081c86073efaab4a14f1d9a256d2c54cd
mysoremasala <krishvinod1024@gmail.com>
2024-01-20 19:39:35 +0530

    hi nigga

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    client_id = "gw6249Y4qNRIIwbleU3NtQ"
-    client_secret = "BtfBfGOy5uK1wQlIhwY6clXR46oetA"
-    username = "maouuuw"
-    password = "immeet44"
+    client_id = "I5ZLnstEk1TW7mvB7NFBdg"
+    client_secret = "iap6KkFvJRnaJT4rZ54gWSOJU6NoEQ"
+    username = "botsaregaefr"
+    password = "robloxfasds13yr_ver"
